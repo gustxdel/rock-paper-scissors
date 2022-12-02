dc3dc0ac3ff8e1b41a7fddf736beb3041425676d
gustxdel <115872141+gustxdel@users.noreply.github.com>
2022-12-02 10:52:31 +0100

    commit -m "Added playround function"

--- main.js
+++ main.js
@@ -2,7 +2,50 @@
 let playerSelection = "Rock";
 let computerSelection = getComputerChoice();
 
+console.log(playRound(playerSelection, computerSelection) );
 
+// Function to play round...............................//
+function playRound(playerSelection, computerSelection) {
+    let answer;
+    //player chooses "Rock"//
+    if (playerSelection === "Rock") {
+        if (computerSelection === "Rock") {
+            answer = "Tie!";    
+        }
+        else if (computerSelection === "Paper") {
+            answer = "You lose! Paper beats Rock";
+        }
+        else if(computerSelection === "Scissors") {
+            answer = "You win! Rock beats Scissors";
+        }
+    }
+    //player chooses "Paper"//
+    else if (playerSelection === "Paper") {
+        if (computerSelection === "Rock") {
+            answer = "You win! Paper beats Rock";    
+        }
+        else if (computerSelection === "Paper") {
+            answer = "Tie!";
+        }
+        else if (computerSelection === "Scissors") {
+            answer = "You lose! Scissors beats Paper";
+        }
+    }
+    //player choose "Scissors"//
+    else if (playerSelection === "Scissors") {
+        if (computerSelection === "Rock") {
+            answer = "You Lose! Rock beats Scissors";    
+        }
+        else if (computerSelection === "Paper") {
+           answer = "You win! Scissors beats Paper";
+        }
+        else if (computerSelection === "Scissors") {
+            answer = "Tie";
+        }
+    }
+
+    return answer;
+}
 
 
 
